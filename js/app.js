f5b58565cb4c7db49d20de8af46fc6944ffc9586
Anthony Webster <anthonyanroab@gmail.com>
2014-08-12 22:59:36 -0600

    ejemplos basicos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 function MyCtrl($scope) {
 	$scope.visible = true;
+	$scope.ambito = "test";
 	$scope.toggle = function() {
 		$scope.visible = !$scope.visible;
 	}
 }
 
 var app = angular.module("myApp",[]);
+
+app.run(function($rootScope){
+	$rootScope.nameTest = "hola mundo";
+})
