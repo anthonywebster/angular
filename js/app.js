0b3d7743721277289998418c37f90d3b0e2e7c67
Anthony Webster <anthonyanroab@gmail.com>
2014-08-13 23:11:28 -0600

    studing

--- js/app.js
+++ js/app.js
@@ -1,13 +1,47 @@
 function MyCtrl($scope) {
 	$scope.visible = true;
+	$scope.val=5;
 	$scope.ambito = "test";
 	$scope.toggle = function() {
 		$scope.visible = !$scope.visible;
 	}
+
+	$scope.getVal = function(){
+		return $scope.val + 1;
+	}
+
+	$scope.name = "";
+	$scope.$watch("name",function(newValue,oldValue){
+		console.log(oldValue);
+		if ($scope.name.length > 0) {
+			$scope.greeting = "Greeting" + $scope.name;
+		};
+	})
 }
 
 var app = angular.module("myApp",[]);
 
 app.run(function($rootScope){
 	$rootScope.nameTest = "hola mundo";
+})
+
+app.factory("UserService",function(){
+	var users = ["peter","daniel","Nina"];
+
+	return {
+		all: function(){
+			return users;
+		},
+		first: function(){
+			return users[0];
+		}
+	}
+});
+
+app.controller("MyCtrl2",function($scope,UserService){
+	$scope.users = UserService.all();
+});
+
+app.controller("AnotherCtrl",function($scope,UserService){
+	$scope.firstUser = UserService.first();
 })
